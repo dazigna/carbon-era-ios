c03797e08a3f0c72c8a519d41fa49ef005e95f9e
Yacine Alami <yacine.alami@gmail.com>
2022-01-13 13:00:17 +0100

    First commit added apollo GraphQl

---
+++ Shared/carboneraApp.swift
@@ -0,0 +1,17 @@
+//
+//  carboneraApp.swift
+//  Shared
+//
+//  Created by Yacine Alami on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct carboneraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
