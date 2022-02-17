70d457e49e4f1b3eb9e094bed3343dcd8ca9d533
Yacine Alami <yacine.alami@gmail.com>
2022-02-17 16:18:16 -0500

    Added color palette and app icon

---
+++ Shared/Extensions/ColorExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  carbonera (iOS)
+//
+//  Created by Yacine Alami on 17/02/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    static let carboneraDark = UIColor(named: "CarboneraDark")
+    static let carboneraGreen = UIColor(named: "CarboneraGreen")
+    static let carboneraBlue = UIColor(named: "CarboneraBlue")
+    static let carboneraWhite = UIColor(named: "CarboneraWhite")
+    static let carboneraGray = UIColor(named: "CarboneraGray")
+}
---
+++ Shared/Views/Statistics/StatItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  StatItemCell.swift
+//  carbonera (iOS)
+//
+//  Created by Yacine Alami on 15/02/2022.
+//
+
+import SwiftUI
+
+struct StatItemCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatItemCell_Previews: PreviewProvider {
+    static var previews: some View {
+        StatItemCell()
+    }
+}
